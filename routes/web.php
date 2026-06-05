11e6c39a4ebd3ddf734292ed07d6c12285268614
Lakshanj97 <116512874+Lakshanj97@users.noreply.github.com>
2026-06-05 20:23:02 +0530

    Create Category part in Admin Panel

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,12 @@ use Illuminate\Support\Facades\Route;
 
 use App\Livewire\Customer\HomePage;
 use App\Livewire\Admin\AdminDashboard;
+use App\Livewire\Admin\CompanyProfile\CompanyProfile;
 
 Route::get('/', HomePage::class)->name('home');
+
 Route::get('/admin/dashboard', AdminDashboard::class)->name('admin.dashboard');
+Route::get('/admin/company-profile', CompanyProfile::class)->name('admin.company-profile');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
